8e75820b25f605d410687ff49c14207ce8bfd7d8
Rajasri Rajkumar <rrajkuma8843@scrippscollege.edu>
2020-08-21 16:20:41 -0500

    connected database to flask app

---
+++ app/models.py
@@ -0,0 +1,36 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    __table_args__ = {
+        'autoload': True,
+        'autoload_with': db.engine
+    }
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    __table_args__ = {
+        'autoload': True,
+        'autoload_with': db.engine
+    }
+
+class Connector(db.Model):
+    __tablename__ = 'connector'
+    __table_args__ = {
+        'autoload': True,
+        'autoload_with': db.engine
+    }
+
+class Goal(db.Model):
+    __tablename__ = 'goals'
+    __table_args__ = {
+        'autoload': True,
+        'autoload_with': db.engine
+    }
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+    __table_args__ = {
+        'autoload': True,
+        'autoload_with': db.engine
+    }
---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom (256)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    CLOUDSQL_USER = 'root'
+    CLOUDSQL_PASSWORD = 'hax4D3People!'
+    CLOUDSQL_DATABASE = 'hackforthepeople'
+    CLOUDSQL_CONNECTION_NAME = 'hackforthepeople:us-central1:hftp'
+
+    SQLALCHEMY_DATABASE_URI = (
+        'mysql+pymysql://{nm}:{pw}@35.238.58.159/{db}').format (
+        nm=CLOUDSQL_USER,
+        pw=CLOUDSQL_PASSWORD,
+        db=CLOUDSQL_DATABASE,
+    )
