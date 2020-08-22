017cf88b8e477c829dbf52c19fe796bf4d2b231e
Snehaskr19 <snehaskr@gmail.com>
2020-08-22 08:10:21 -0700

    merged code

--- app/mng_goals.py
+++ app/mng_goals.py
@@ -2,18 +2,20 @@ from app import app
 from app import db
 from app.models import User, Restaurant, Goal, Category, Connector
 
+
 def get_all_goals():
     return Goal.query \
         .join(Category, Goal.categoryId == Category.categoryId) \
         .add_columns(Goal.goalId, Goal.goalName, Category.categoryName) \
         .all()
 
+
 def populate_connector(user_id):
     """
         when user registers, all the static goals for the user are automatically set to have a status of 0
     """
     goals = get_all_goals()
-    
+
     restaurant_id = User.query.filter_by(userId=user_id).first().restaurantId
 
     for goal in goals:
@@ -21,6 +23,12 @@ def populate_connector(user_id):
         db.session.add(connector)
         db.session.commit()
 
-        
-    
+def generate_report():
 
+    category_1 = []
+    category_2 = []
+    category_3 = []
+    category_4 =[]
+    category_5 = [3, 5, 4, 5, 3]
+    goals = user_goals()
+    for goal in goals:
