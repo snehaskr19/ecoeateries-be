a1752def1d8371cc6f1daed1096b6b8fad56b29a
Snehaskr19 <snehaskr@gmail.com>
2020-08-22 08:03:22 -0700

    report code

---
+++ app/gen_report.py
@@ -0,0 +1,12 @@
+from mng_goals import user_goals
+
+category_1 = []
+category_2 = []
+category_3 = []
+category_4 =[]
+category_5 = [3, 5, 4, 5, 3]
+
+def generate_report():
+    goals = user_goals()
+    for goal in goals:
+
